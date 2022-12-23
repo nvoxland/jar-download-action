535fea1fd0ec01119b360ed612b3bf56d52c1c43
Nathan Voxland <nathan@voxland.net>
2022-12-23 09:32:01 -0600

    Initial commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,30 @@
 #!/bin/sh -l
 
-cd ${{ env.GITHUB_WORKSPACE }}
 
-mvn -B -f /builder.pom.xml  dependency:copy-dependencies -s /usr/share/maven/ref/settings-docker.xml "$@"
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --artifactId=*)
+      $artifactId="${1#*=}"
+      ;;
+    --groupId=*)
+      groupId="${1#*=}"
+      ;;
+    --version=*)
+      version="${1#*=}"
+      ;;
+    --outputDirectory=*)
+      outputDirectory="${1#*=}"
+    *)
+      printf "***************************\n"
+      printf "* Error: Invalid argument.*\n"
+      printf "***************************\n"
+      exit 1
+  esac
+  shift
+done
+
+mvn -B -f /builder.pom.xml  dependency:copy-dependencies -s /usr/share/maven/ref/settings-docker.xml \
+  -DoutputDirectory=$(pwd)/$outputDirectory \
+  -Dwanted.groupId=${groupId} \
+  -Dwanted.artifactId=${artifactId} \
+  -Dwanted.version=${version}
